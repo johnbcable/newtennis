8b1ab9dc5aa7b61780f77752c7ea038ed78beb84
John Cable <johnbcable@me.com>
2019-12-27 18:14:39 +0000

    Xmas add-ins

--- www/js/app.js
+++ www/js/app.js
@@ -2,3 +2,8 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 
 $(document).foundation();
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
